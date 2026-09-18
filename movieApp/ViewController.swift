8d0e05ba6a65e87f2479e2ce26e62eea8ff19e29
avazcafarov-creator <avazcafarov@gmail.com>
2026-09-18 14:45:22 +0400

    Initial Commit

---
+++ movieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Avaz Cafarov on 18.09.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
